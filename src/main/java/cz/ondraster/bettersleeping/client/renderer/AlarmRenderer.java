30958925015490a37b66a000230d58f79d5f37f9
OndraSter <ondra.moravek@hotmail.com>
2015-01-18 19:25:49 +0100

    Some.. more attempts at rendering time overlay (still does not work)

--- src/main/java/cz/ondraster/bettersleeping/client/renderer/AlarmRenderer.java
+++ src/main/java/cz/ondraster/bettersleeping/client/renderer/AlarmRenderer.java
@@ -44,34 +44,43 @@ public class AlarmRenderer extends TileEntitySpecialRenderer {
       GL11.glRotatef(-rotation, 0f, 1.0f, 0f);
       tm.bindTexture(new ResourceLocation(BetterSleeping.MODID, "textures/alarm.png"));
       modelAlarm.render();
+      GL11.glPopMatrix();
+      /*GL11.glPushMatrix();
+      GL11.glLoadIdentity();
 
-
-/*      tm.bindTexture(new ResourceLocation(BetterSleeping.MODID, "textures/gui/alarm.png"));
+      tm.bindTexture(new ResourceLocation(BetterSleeping.MODID, "textures/gui/numbersx.png"));
       MinecraftTime time = MinecraftTime.getFromWorldTime(te.getWorldObj().getWorldTime());
 
       // render 10s of hours
       int nbr = time.getRealHour() % 10;
-      GL11.glTranslatef((float) x, (float) y + 4f, (float) z);
-      GL11.glRotatef(180f, 0f, 1.0f, 0f);
-      drawCharacter(x, y, z, nbr);*/
-      GL11.glPopMatrix();
+      //GL11.glTranslatef((float) x - 0.15f, (float) y + 4.1f, (float) z);
+      GL11.glTranslatef((float) x + 0.5F, (float) y + 1.5F, (float) z + 0.5F);
+      GL11.glRotatef(180f, 0f, 0f, 1f);
+      GL11.glRotatef(-rotation, 0f, 1.0f, 0f);
+      if (meta == 3) {
+         GL11.glRotatef(-30f, 1f, 0f, 0f);
+      }
+      drawCharacter(x, y, z, nbr);
+      GL11.glPopMatrix();*/
    }
 
    private void drawCharacter(double x, double y, double z, int character) {
-      double width = 8 / 128d;
-      double height = 8 / 64d;
-      double u = 16 / 256d;
-      double v = 103 / 256d;
-      double nmrWidth = 9 / 256d;
+      double width = 8 / 20d;
+      double height = 0.5d;
+      double u = 0 / 100d;
+      double v = 0 / 17d;
+      double nmrWidth = 9 / 99d;
       Tessellator tessellator = Tessellator.instance;
+      double renderWidth = 0.2;
+      double renderHeight = 0.5;
 
       tessellator.startDrawingQuads();
 
       u += nmrWidth * 4;
       tessellator.addVertexWithUV(x, y, z, u + width, v);
-      tessellator.addVertexWithUV(x + 0.5, y, z, u, v);
-      tessellator.addVertexWithUV(x + 0.5, y + 0.5, z, u, v + height);
-      tessellator.addVertexWithUV(x, y + 0.5, z, u + width, v + 0.5);
+      tessellator.addVertexWithUV(x + renderWidth, y, z, u, v);
+      tessellator.addVertexWithUV(x + renderWidth, y + renderHeight, z, u, v + height);
+      tessellator.addVertexWithUV(x, y + renderHeight, z, u + width, v + height);
 
       tessellator.draw();
    }
