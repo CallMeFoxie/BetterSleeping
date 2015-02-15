806762e0310bbcfed6fbd020c8b600dfbd082f55
OndraSter <ondra.moravek@hotmail.com>
2015-02-15 16:55:36 +0100

    Some test work on #3 - still not finished

--- src/main/java/cz/ondraster/bettersleeping/client/renderer/AlarmRenderer.java
+++ src/main/java/cz/ondraster/bettersleeping/client/renderer/AlarmRenderer.java
@@ -26,16 +26,16 @@ public class AlarmRenderer extends TileEntitySpecialRenderer {
       GL11.glPushMatrix();
 
       int meta = te.getBlockMetadata();
-      int rotation = 0, rotationText = 180;
+      int rotation = 0, rotationText = 0;
       if (meta == 3) {
          rotation = 180;
-         rotationText = 0;
+         rotationText = 180;
       } else if (meta == 4) {
          rotation = 90;
-         rotationText = 180;
+         rotationText = 90;
       } else if (meta == 5) {
          rotation = -90;
-         rotationText = 180;
+         rotationText = -90;
       }
 
 
@@ -45,42 +45,47 @@ public class AlarmRenderer extends TileEntitySpecialRenderer {
       tm.bindTexture(new ResourceLocation(BetterSleeping.MODID, "textures/alarm.png"));
       modelAlarm.render();
       GL11.glPopMatrix();
-      /*GL11.glPushMatrix();
-      GL11.glLoadIdentity();
+      /*
+      GL11.glPushMatrix();
+      //GL11.glLoadIdentity();
 
-      tm.bindTexture(new ResourceLocation(BetterSleeping.MODID, "textures/gui/numbersx.png"));
+      tm.bindTexture(new ResourceLocation(BetterSleeping.MODID, "textures/gui/numbers.png"));
       MinecraftTime time = MinecraftTime.getFromWorldTime(te.getWorldObj().getWorldTime());
 
       // render 10s of hours
       int nbr = time.getRealHour() % 10;
       //GL11.glTranslatef((float) x - 0.15f, (float) y + 4.1f, (float) z);
-      GL11.glTranslatef((float) x + 0.5F, (float) y + 1.5F, (float) z + 0.5F);
+      //GL11.glTranslatef((float) x + 0.5F, (float) y + 1.5F, (float) z + 0.5F);
+      GL11.glDepthMask(false);
       GL11.glRotatef(180f, 0f, 0f, 1f);
-      GL11.glRotatef(-rotation, 0f, 1.0f, 0f);
+      GL11.glRotatef(-rotationText, 0f, 1.0f, 0f);
+      //GL11.glRotatef(-rotation, 0f, 1.0f, 0f);
       if (meta == 3) {
-         GL11.glRotatef(-30f, 1f, 0f, 0f);
+         //GL11.glRotatef(-30f, 1f, 0f, 0f);
       }
+      //GL11.glNormal3f(0.0F, 0.0F, 0.0F);
+      GL11.glDepthMask(true);
       drawCharacter(x, y, z, nbr);
       GL11.glPopMatrix();*/
    }
 
    private void drawCharacter(double x, double y, double z, int character) {
-      double width = 8 / 20d;
-      double height = 0.5d;
+      double width = 9 / 100d;
+      double height = 1d;
       double u = 0 / 100d;
       double v = 0 / 17d;
-      double nmrWidth = 9 / 99d;
+      double nmrWidth = 9 / 100d;
       Tessellator tessellator = Tessellator.instance;
       double renderWidth = 0.2;
       double renderHeight = 0.5;
 
       tessellator.startDrawingQuads();
 
-      u += nmrWidth * 4;
-      tessellator.addVertexWithUV(x, y, z, u + width, v);
-      tessellator.addVertexWithUV(x + renderWidth, y, z, u, v);
-      tessellator.addVertexWithUV(x + renderWidth, y + renderHeight, z, u, v + height);
-      tessellator.addVertexWithUV(x, y + renderHeight, z, u + width, v + height);
+      //u += nmrWidth * 4;
+      tessellator.addVertexWithUV(x, y, z, u, v);
+      tessellator.addVertexWithUV(x + renderWidth, y, z, u + width, v);
+      tessellator.addVertexWithUV(x + renderWidth, y + renderHeight, z, u + width, v + height);
+      tessellator.addVertexWithUV(x, y + renderHeight, z, u, v + height);
 
       tessellator.draw();
    }
