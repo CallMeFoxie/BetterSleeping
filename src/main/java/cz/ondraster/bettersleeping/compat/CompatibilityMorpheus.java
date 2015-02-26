4056108ef355f3cb6ed8b2373e0dbb5e4c292002
OndraSter <ondra.moravek@hotmail.com>
2015-02-26 19:58:12 +0100

    Disable custom sleeping checker when Morpheus is installed

--- src/main/java/cz/ondraster/bettersleeping/compat/CompatibilityMorpheus.java
+++ src/main/java/cz/ondraster/bettersleeping/compat/CompatibilityMorpheus.java
@@ -2,6 +2,7 @@ package cz.ondraster.bettersleeping.compat;
 
 import cpw.mods.fml.common.FMLLog;
 import cz.ondraster.bettersleeping.BetterSleeping;
+import cz.ondraster.bettersleeping.Config;
 import cz.ondraster.bettersleeping.logic.Alarm;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.World;
@@ -21,6 +22,9 @@ public class CompatibilityMorpheus implements INewDayHandler {
       } catch (Exception e) {
          FMLLog.warning("[" + BetterSleeping.MODID + "] Failed to load the Morpheus compatibility!");
       }
+
+      FMLLog.info("[" + BetterSleeping.MODID + "] Morpheus compat seems to have loaded, disabling custom sleeping code.");
+      Config.percentPeopleToSleep = 2d;
    }
 
    @Override
