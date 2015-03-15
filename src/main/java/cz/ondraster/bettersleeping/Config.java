c5637e82bb633b68e46c02324fb60b6d95f5806a
OndraSter <ondra.moravek@hotmail.com>
2015-03-15 21:48:52 +0100

    Cleaned up config

--- src/main/java/cz/ondraster/bettersleeping/Config.java
+++ src/main/java/cz/ondraster/bettersleeping/Config.java
@@ -8,48 +8,50 @@ import net.minecraftforge.common.config.Configuration;
 import java.io.File;
 
 public class Config {
-   // config
-   public static int defaultWakeUpTime = 0;
-   public static int oversleepWithoutAlarm = 2000;
-   public static int oversleepWithAlarm = 400;
-   public static boolean enableSleepCounter = true;
-   public static long spawnSleepCounter = 18000;
-   public static int ticksPerSleepCounter = 4;
-   public static double sleepPerSleptTick = 0.5;
-   public static int maximumSleepCounter = 18000;
+   // ALARM
+   public static int defaultWakeUpTime = 0;           // daytime offset
+   public static int oversleepWithoutAlarm = 2000;    // how much can you oversleep without alarm
+   public static int oversleepWithAlarm = 400;        // how much can you oversleep with alarm
 
-   public static double percentPeopleToSleep = 0.5;
+   // MAX
+   public static int maximumSleepCounter = 18000;     // how much tiredness you can get the most
+   public static long spawnSleepCounter = 18000;      // how much tiredness you are spawned in
 
-   public static boolean enableRingWatch = true;
-   public static boolean enableAlarmClock = true;
+   // BLOCKS & ITEMS
+   public static boolean enableRingWatch = true;      // enable ring watch
+   public static boolean enableAlarmClock = true;     // enable alarm clock
 
-   public static boolean enableAlarmSound = true;
+   // DRAIN
+   public static int ticksPerSleepCounter = 4;        // how many ticks it takes to add tiredness
+   public static int tirednessJump = 8;               // how many tiredness is added on a jump
 
-   public static boolean enableSleepTicks = false;
-
-   public static boolean resetCounterOnDeath = true;
-
+   // GAIN
+   public static double sleepPerSleptTick = 0.5;      // how much tiredness is removed on a slept tick
+   public static int giveSleepCounterOnSleep = ticksPerSleepCounter; // how much tiredness is added when you lay in bed
 
    // debuff related stuff
-   public static boolean enableDebuffs = true;
-   public static boolean sleepOnGround = true;
+   public static boolean enableDebuffs = true;        // enable debuffs
+   public static boolean sleepOnGround = true;        // sleep on ground
 
    // tweaks
-   public static boolean enablePositionReset = true;
-   public static double chanceToStopRain = 0.9;
-   public static boolean disableSleeping = false;
-
+   public static boolean enablePositionReset = true;  // enable position reset when sleeping on ground
+   public static double chanceToStopRain = 0.9;       // chance to stop rain
+   public static boolean disableSleeping = false;     // disable sleeping alltogether
+   public static boolean resetCounterOnDeath = true;  // reset counter on death
+   public static boolean enableSleepTicks = false;    // enable tick simulation when asleep
+   public static double percentPeopleToSleep = 0.5;   // How many people need to sleep
+   public static boolean enableSleepCounter = true;   // enable sleep counter at all
 
    // gui
-   public static int guiOffsetLeft = 4;
-   public static int guiOffsetTop = 8;
-   public static boolean enableSleepyBar = true;
+   public static int guiOffsetLeft = 4;               // sleepybar offset left
+   public static int guiOffsetTop = 8;                // sleepybar offset top
+   public static boolean enableSleepyBar = true;      // display sleepybar at all
+   public static boolean enableAlarmSound = true;     // play sound on woken up by alarm
+   public static boolean enableSleepMessage = true;   // enable sleep message related to how many people need to sleep
 
-
-   // private config stuff
-   public static final int POTION_DURATION = 40;
-   public static int giveSleepCounterOnSleep = ticksPerSleepCounter;
-   public static boolean enableSleepMessage = true;
+   // PRIVATE
+   public static final int POTION_DURATION = 40;      // duration of potion effect in ticks [INTERNAL]
+   
 
    private Configuration cfg;
 
